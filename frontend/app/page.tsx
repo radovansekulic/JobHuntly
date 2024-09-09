5fa6c935616a171f93c04f1536eb62cdb4953838
radovansekulic <radovan.sekuliic@gmail.com>
2024-09-09 06:54:26 -0700

    Created register page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -75,7 +75,7 @@ export default function Home() {
             <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Discover more than 5000+ Jobs</h1>
             <p className="mt-6 text-lg leading-8 text-gray-600">The freelance job platform connects freelancers with global clients, offering projects that match their skills and interests.</p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
-              <a href="#" className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Register Now</a>
+              <a href={"/register"} className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Register Now</a>
               <a href="#" className="text-sm font-semibold leading-6 text-gray-900">Sign In <span aria-hidden="true">→</span></a>
             </div>
           </div>
